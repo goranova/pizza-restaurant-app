8b6472a0ac435df322eb9cd233d39f81925e242f
goranova <tsvety_goranova@abv.bg>
2023-12-15 15:05:54 +0200

    First Commit

---
+++ src/main/java/com/finbryte/pizzarestaurant/PizzarestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.finbryte.pizzarestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzarestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzarestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finbryte/pizzarestaurant/PizzarestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finbryte.pizzarestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzarestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
